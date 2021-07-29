28ff47a63403470c07f663a9feb8ceb907e59829
Gabriel Ybarra <ybarr015@cougars.csusm.edu>
2021-07-28 21:33:39 -0700

    update secret key line

--- Spotify_Django/Spotify_Django/settings.py
+++ Spotify_Django/Spotify_Django/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from Django_Secret import SECRET
+from .Django_Secret import SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
